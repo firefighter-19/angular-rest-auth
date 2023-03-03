e8678a7e6ea9069388f502b0206e65a715349f5c
Andrey Sazonov <fromlhz03@gmail.com>
2023-03-03 20:27:22 +0200

    add local storage service

---
+++ src/app/shared/services/local-storage/local-storage.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from "@angular/core";
+
+@Injectable({
+  providedIn: "root",
+})
+export class LocalStorageService {
+  hasLocalStorage!: boolean;
+  private readonly storageKeyAccessToken = "secretAuthToken";
+
+  constructor() {
+    this.checkLocalStorage();
+  }
+
+  private saveData<T>(key: string, data: T): void {
+    if (!this.hasLocalStorage) {
+      return;
+    }
+    localStorage.setItem(key, JSON.stringify(data));
+  }
+
+  getData<T>(key: string): T | null {
+    if (!this.hasLocalStorage) {
+      return null;
+    }
+    return JSON.parse(localStorage.getItem(key) || "");
+  }
+
+  getString(key: string): string | null {
+    if (!this.hasLocalStorage) {
+      return null;
+    }
+    return localStorage.getItem(key);
+  }
+
+  private checkLocalStorage(): void {
+    try {
+      const test = "t";
+      localStorage.setItem(test, test);
+      localStorage.removeItem(test);
+      this.hasLocalStorage = true;
+    } catch (e) {
+      this.hasLocalStorage = false;
+    }
+  }
+
+  updateLocalStorageData(authToken: string): void {
+    this.saveData(this.storageKeyAccessToken, authToken);
+  }
+}
