6ebb5e8dc0748dcba3e1eadcc3c6499365f56ff5
Andrey Sazonov <fromlhz03@gmail.com>
2023-03-03 17:34:23 +0200

    install ngrx

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,15 @@ import { provideRouter } from "@angular/router";
 import { AppComponent } from "./app/app.component";
 import { appRoutes } from "./app/app.route";
 import { bootstrapApplication } from "@angular/platform-browser";
+import { provideHttpClient } from "@angular/common/http";
+import { StoreModule } from "@ngrx/store";
+import { reducers } from "./app/shared/data-access";
+import { importProvidersFrom } from "@angular/core";
 
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter(appRoutes)],
-});
+  providers: [
+    provideRouter(appRoutes),
+    provideHttpClient(),
+    importProvidersFrom(StoreModule.forRoot(reducers)),
+  ],
+}).catch((err) => console.error(err));
