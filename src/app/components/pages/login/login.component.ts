b1d0a1b94cf2cfdd43948a989e62a780cb042626
Andrey Sazonov <fromlhz03@gmail.com>
2023-03-03 20:26:29 +0200

    add ui to login component, add dispatch login user

--- src/app/components/pages/login/login.component.ts
+++ src/app/components/pages/login/login.component.ts
@@ -1,13 +1,47 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, OnInit } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from "@angular/forms";
+import { UiInputComponent } from "src/app/shared/ui-kit/ui-input/ui-input/ui-input.component";
+import { Store } from "@ngrx/store";
+import { authActions } from "src/app/shared/data-access/auth";
 
 @Component({
-  selector: 'app-login',
+  selector: "app-login",
   standalone: true,
-  imports: [CommonModule],
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  imports: [CommonModule, ReactiveFormsModule, UiInputComponent],
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  form!: FormGroup<{
+    username: FormControl<string | null>;
+    password: FormControl<string | null>;
+  }>;
 
+  constructor(private readonly store: Store) {}
+
+  ngOnInit(): void {
+    this.initForm();
+  }
+
+  private initForm(): void {
+    this.form = new FormGroup({
+      username: new FormControl("", [Validators.required]),
+      password: new FormControl("", Validators.required),
+    });
+  }
+
+  onSignIn() {
+    const { username, password } = this.form.value as {
+      username: string;
+      password: string;
+    };
+    this.store.dispatch(authActions.tryAuthAction({ username, password }));
+  }
 }
