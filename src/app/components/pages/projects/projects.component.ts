a46b8a74d8bc6443c4324beeace8498a5367e77f
Andrey Sazonov <fromlhz03@gmail.com>
2023-03-04 16:05:17 +0200

    add accordion to main page

--- src/app/components/pages/projects/projects.component.ts
+++ src/app/components/pages/projects/projects.component.ts
@@ -1,12 +1,12 @@
-import { CategoriesComponent } from "./../categories/categories.component";
 import { RouterModule } from "@angular/router";
 import { Component } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { PanelComponent } from "../../ui/panel/panel.component";
 
 @Component({
   selector: "app-projects",
   standalone: true,
-  imports: [CommonModule, RouterModule, CategoriesComponent],
+  imports: [CommonModule, RouterModule, PanelComponent],
   templateUrl: "./projects.component.html",
   styleUrls: ["./projects.component.css"],
 })
