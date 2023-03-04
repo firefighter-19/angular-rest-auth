3d9c66f77f118adc8d8ccf5346eaf17d04b352c2
Andrey Sazonov <fromlhz03@gmail.com>
2023-03-04 12:34:29 +0200

    upd projects route

--- src/app/routes/projects.route.ts
+++ src/app/routes/projects.route.ts
@@ -1,6 +1,6 @@
 import { Routes } from "@angular/router";
 import { ProjectsComponent } from "../components/pages/projects/projects.component";
-import { ProjectComponent } from "../components/pages/ui/project/project.component";
+import { ProjectComponent } from "../components/ui/project/project.component";
 
 export const projectRoutes: Routes = [
   {
