6f79ab216a38a9d8b0dbbd09874e5e5a7d63e6d8
Andrey Sazonov <fromlhz03@gmail.com>
2023-03-04 16:24:01 +0200

    add mat-button to header component

--- src/app/core-ui/components/header/header.component.ts
+++ src/app/core-ui/components/header/header.component.ts
@@ -1,11 +1,12 @@
 import { RouterModule } from "@angular/router";
 import { CommonModule } from "@angular/common";
 import { ChangeDetectionStrategy, Component } from "@angular/core";
+import { MatButtonModule } from "@angular/material/button";
 
 @Component({
   selector: "app-header",
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, MatButtonModule],
   templateUrl: "./header.component.html",
   styleUrls: ["./header.component.css"],
   changeDetection: ChangeDetectionStrategy.OnPush,
