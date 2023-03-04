5e60f247dccc5ebe26ffa302cacc37599da01691
Andrey Sazonov <fromlhz03@gmail.com>
2023-03-04 12:33:05 +0200

    change project folder

--- src/app/components/pages/projects/projects.component.ts
+++ src/app/components/pages/projects/projects.component.ts
@@ -1,12 +1,12 @@
-import { ActivatedRoute, RouterModule } from "@angular/router";
+import { CategoriesComponent } from "./../categories/categories.component";
+import { RouterModule } from "@angular/router";
 import { Component } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { ProjectComponent } from "../ui/project/project.component";
 
 @Component({
   selector: "app-projects",
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, CategoriesComponent],
   templateUrl: "./projects.component.html",
   styleUrls: ["./projects.component.css"],
 })
