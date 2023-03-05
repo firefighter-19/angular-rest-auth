52a894dcd602397a13f8578f8601fdb053c45af0
Andrey Sazonov <fromlhz03@gmail.com>
2023-03-05 15:30:50 +0200

    add products component inside projects component

--- src/app/components/pages/projects/projects.component.ts
+++ src/app/components/pages/projects/projects.component.ts
@@ -1,16 +1,17 @@
 import { RouterModule } from "@angular/router";
-import { Component } from "@angular/core";
+import { ChangeDetectionStrategy, Component } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { PanelComponent } from "../../ui/panel/panel.component";
+import { ProductsComponent } from "./products/products.component";
 
 @Component({
   selector: "app-projects",
   standalone: true,
-  imports: [CommonModule, RouterModule, PanelComponent],
+  imports: [CommonModule, RouterModule, PanelComponent, ProductsComponent],
   templateUrl: "./projects.component.html",
   styleUrls: ["./projects.component.css"],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ProjectsComponent {
   id: string = "1";
-  constructor() {}
 }
