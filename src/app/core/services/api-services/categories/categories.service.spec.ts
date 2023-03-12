37230b7f2f683c6c9d1c6021e49eb1c91d783abf
Andrey Sazonov <fromlhz03@gmail.com>
2023-03-12 11:00:27 +0200

    add tests for categories, products requests

--- src/app/core/services/api-services/categories/categories.service.spec.ts
+++ src/app/core/services/api-services/categories/categories.service.spec.ts
@@ -1,16 +1,39 @@
-import { TestBed } from '@angular/core/testing';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { defer } from 'rxjs';
+import { HttpService } from '../http/http.service';
 
 import { CategoriesService } from './categories.service';
 
 describe('CategoriesService', () => {
   let service: CategoriesService;
+  let httpClientSpy: jasmine.SpyObj<HttpClient>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(CategoriesService);
+    const getHeaders = {
+      get headers() {
+        return new HttpHeaders({
+          "Content-Type": "application/json",
+        });
+      },
+    };
+    httpClientSpy = jasmine.createSpyObj("HttpClient", ["get"]);
+    service = new CategoriesService(httpClientSpy, getHeaders as HttpService);
+
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('should return list of categories', (done: DoneFn) => {
+    const categories = ['Phone', 'Microwaves']
+    httpClientSpy.get.and.returnValue(
+      defer(() => Promise.resolve(categories))
+    );
+
+    service.getAllCategories().subscribe({
+      next: (value) => {
+          expect(value).withContext('categories').toEqual(categories);
+          done();
+      },
+      error: done.fail
+    })
+    expect(httpClientSpy.get.calls.count()).withContext("one call").toBe(1);
   });
 });
--- src/app/core/services/api-services/products/products.service.spec.ts
+++ src/app/core/services/api-services/products/products.service.spec.ts
@@ -1,16 +1,95 @@
-import { TestBed } from "@angular/core/testing";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { defer } from "rxjs";
+import { IProductsWithCategory } from "src/app/core/dto/category-products.dto";
+import {
+  IProducts,
+  IProductsPaginationOptions,
+} from "src/app/core/interfaces/products";
+import { HttpService } from "../http/http.service";
 
 import { ProductsService } from "./products.service";
 
 describe("ProductsService", () => {
   let service: ProductsService;
+  let httpClientSpy: jasmine.SpyObj<HttpClient>;
+  const params: IProductsPaginationOptions = {
+    limit: 10,
+    skip: 0,
+  };
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(ProductsService);
+    const getHeaders = {
+      get headers() {
+        return new HttpHeaders({
+          "Content-Type": "application/json",
+        });
+      },
+    };
+    httpClientSpy = jasmine.createSpyObj("HttpClient", ["get"]);
+    service = new ProductsService(httpClientSpy, getHeaders as HttpService);
   });
 
-  it("should be created", () => {
-    expect(service).toBeTruthy();
+  it("should return products list", (done: DoneFn) => {
+    const products: IProducts = {
+      total: 10,
+      products: [
+        {
+          id: 1,
+          category: "Category",
+          description: "Description",
+          discountPercentage: 10,
+          images: ["url"],
+          price: 100,
+          rating: 5,
+          stock: 100,
+          thumbnail: "thumbnail",
+          title: "Title",
+        },
+      ],
+    };
+    httpClientSpy.get.and.returnValue(defer(() => Promise.resolve(products)));
+
+    service.getAllProducts(params).subscribe({
+      next: (value) => {
+        expect(value).withContext("products").toEqual(products);
+        done();
+      },
+      error: done.fail,
+    });
+    expect(httpClientSpy.get.calls.count()).withContext("one call").toBe(1);
+  });
+
+  it("should return products list of particular category", (done: DoneFn) => {
+    const paramsWithCategory: IProductsWithCategory = {
+      ...params,
+      categoryName: "Category",
+    };
+    const products: IProducts = {
+      total: 10,
+      products: [
+        {
+          id: 1,
+          category: "Category",
+          description: "Description",
+          discountPercentage: 10,
+          images: ["url"],
+          price: 100,
+          rating: 5,
+          stock: 100,
+          thumbnail: "thumbnail",
+          title: "Title",
+        },
+      ],
+    };
+    httpClientSpy.get.and.returnValue(defer(() => Promise.resolve(products)));
+
+    service.getProductsOfACategory(paramsWithCategory).subscribe({
+      next: (value) => {
+        expect(value).withContext("products").toEqual(products);
+        done();
+      },
+      error: done.fail,
+    });
+    expect(httpClientSpy.get.calls.count()).withContext("one call").toBe(1);
   });
 });
