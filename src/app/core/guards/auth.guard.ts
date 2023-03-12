d1a1fd97ede495acba834af06f35806c8cd37354
Andrey Sazonov <fromlhz03@gmail.com>
2023-03-12 13:17:40 +0200

    rewrite guard

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -1,9 +1,18 @@
+import { Router } from "@angular/router";
+import { LocalStorageService } from "src/app/core/services/local-storage/local-storage.service";
 import { inject } from "@angular/core";
-import { Store } from "@ngrx/store";
 import { authSelectors } from "src/app/shared/data-access/auth";
 
 /*
   Unfortunately dummyjson api doesn't allow us to check our token validity, so we will just check its existing in local storage.
 */
-export const AuthGuard = (store = inject(Store)) => store.select(authSelectors.selectIsAuth)
-
+export const AuthGuard = (
+  localStorage = inject(LocalStorageService),
+  router = inject(Router)
+) => {
+  if (localStorage.getData("Authorization")) {
+    return true;
+  }
+  router.navigate(["/auth"]);
+  return false;
+};
